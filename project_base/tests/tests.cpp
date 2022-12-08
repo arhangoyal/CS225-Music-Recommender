82a72d33180f727e06d4d50a50d75c7138ec3a02
Lawlithon <80422301+Lawlithon@users.noreply.github.com>
2022-12-08 15:42:56 -0600

    Add files via upload
    renamed files, added a data reader for airport data, implemented distance calculator. issue where writing each string in a vector results in the 0th index being missing

--- project_base/tests/tests.cpp
+++ project_base/tests/tests.cpp
@@ -1,3 +1,11 @@
-#include "reddit.h"
+#include "flights.h"
 
 #include <catch2/catch_test_macros.hpp>
+
+// test if any vector isnt size 4
+
+// test if first is source, second is source ID, third is dest, fourth is dest ID
+
+// test distance calculator
+
+// test radian converter
